392d467463376ea47563437d8670748de2ccc6c3
MUHAMMAD ADIL RAZA <ary18sw18@gmail.com>
2023-09-16 14:06:46 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import React from 'react';
+import {AccountProvider} from "./contexts/AccountContext";
+import {XRPLClientProvider} from "./contexts/XRPLClientContext";
+import NavBar from './components/NavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TransferPage from './pages/TransferPage';
+import { AccountInfoProvider } from './contexts/AccountInfoContext';
+import TradePage from './pages/TradePage/TradePage';
+import { OfferProvider } from './contexts/OfferContext';
+import MyOffersPage from './pages/MyOffersPage';
+import { MyOfferProvider } from './contexts/MyOfferContext';
+
+function App() {
+  // localStorage.clear();
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <XRPLClientProvider>
+            <AccountProvider>
+              <AccountInfoProvider>
+                    <OfferProvider>
+                      <MyOfferProvider>
+
+                        <NavBar></NavBar>
+
+                        <Routes>
+                          <Route path="/" element={<HomePage />} />
+                          <Route path="transfer" element={<TransferPage />} />
+                          <Route path="trade" element={<TradePage />} />
+                          <Route path="my_offers" element={<MyOffersPage />} />
+                        </Routes>
+
+                      </MyOfferProvider>
+                    </OfferProvider>
+              </AccountInfoProvider>
+            </AccountProvider>
+        </XRPLClientProvider>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+// 
+//
